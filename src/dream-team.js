2a839f4ce633c9fdf5508dd4ff0aec212573e47d
RomanShajnoga <shajnogar@gmail.com>
2020-09-25 14:48:38 +0300

    feat: add dream-team.js

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,16 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(member) {
+  // throw new CustomError('Not implemented');
+  if (!Array.isArray(member)) {
+    return false;
+  }
+
+  const result = member
+  .filter(el => el && typeof el === 'string')
+  .map(el => el.trim().toUpperCase())
+  .sort()
+  .reduce((accumulator, element) => (accumulator += element[0]), '');
+
+return result ? result : false;
 };
